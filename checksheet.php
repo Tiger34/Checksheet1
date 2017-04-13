5335bc2375d3872086db2e35bdcda93ae69085b9
Tiger34 <justinwatkins114@icloud.com>
2017-04-12 20:52:24 -0400

    work
    my work

---
+++ checksheet.php
@@ -0,0 +1,202 @@
+
+<!doctype html>
+<html>
+<head>  
+<meta charset="utf-8">
+
+
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+
+<!-- jQuery library -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
+
+<!-- Latest compiled JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+
+<title>Gear List</title>
+
+
+<style>
+	
+body{
+
+background-color: #808080;
+
+}
+
+
+</style>
+
+
+</head>
+
+<body>
+
+
+
+<div class="container-fluid">
+
+<div class="row">
+
+<div class="col-xs-4 col-sm-4 col-md-4 col-lg-4"></div>
+
+<div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
+
+<?php date("Y/m/d"); ?>
+
+
+<h1> Personnel </h1>
+
+<div>RSO Letters onfile</div>
+
+<div>Shooter's list</div>1
+
+<div>Departure time</div>
+<div>Return time</div>
+<div>Ammo availability</div>
+<div>Range set-up time</div>
+<div>Range clean-up time</div>
+
+
+<h1>Tools</h1>
+<div>Request permission</div>
+<div>Schedule/reserve the range</div>
+<div>Schedule/reserve transportation</div>
+<div>Ammo requisition</div>
+<div>Announce the event</div>
+<div>Post maps and directions</div>
+<div>Pre-fires</div>
+<div>Log the weapons out</div>
+<div>SAMI book</div>
+<div>Safety brief</div>
+<div>Round count log books</div>
+<div>Muster sheets</div>
+<div>Score sheets (3591)</div>
+<div>Check weather reports</div>
+<div>Foul weather gear</div>
+<div>BRAVO flag</div>
+<div>Chow arrangements</div>
+
+
+<div>RSO</div>1
+<div>Range Safety</div>1
+<div>Line Coaches</div>1
+<div>Trainees</div>1
+<div>Beach Guard</div>1
+<div>Gate Guard</div>1
+<div>Duty Driver</div>1
+<div>Mag Loaders</div>1
+
+
+<div>Ammunition</div>
+<div>Weapons</div>
+<div>Magazines</div>
+<div>M9: 1005-01-204-4376</div>
+<div>M16: 1005-00-921-5004</div>
+<div>M14: 1005-00-628-9048</div>
+<div>Holsters</div>
+<div>Belts</div>
+<div>Rifle mats</div>
+<div>Slings</div>
+<div>Targets Targets</div>
+<div>Rifle "A" Target: 6920-00-627-4071</div>
+<div>25M Zeroing: 6920-01-253-4005</div>
+<div>"E" Silhouette Target: 6920-00-795-1806</div>
+<div>10M Machine Gun: 6920-00-078-5123</div>
+<div>Pasters Black and White</div>
+<div>5,000 count:</div>
+<div>Pasters, Black: 6920-00-165-6354</div>
+<div>Pasters, White: 6920-00-172-3572</div>
+<div>Eye protection</div>
+<div>Ear protection</div>
+<div>Foamies</div>
+<div>Range gear bags</div>
+<div>Radios</div>
+<div>Stop watch</div>
+<div>Flashlights</div>
+<div>Mega phone  Batteries</div>
+<div>Gloves</div>
+<div>Speed Loaders</div>
+<div>Spray paint (black, green)</div>
+<div>Spray adhesive</div>
+<div>CLP Cleaning, Lubricant, Prevent</div>
+<div>G96 Brand: 4 oz: 9150-01-079-6124</div>
+<div>1 Pint with sprayer: 9150-01-054-6453</div>
+<div>WD-40 </div> 
+<div>Masking tape</div>
+<div>Markers</div>
+<div>Pens, Pencils, Clipboards Pens, Pencils, Clipboards</div>
+<div>Line coaches need pens, black markers</div>
+<div>SAMI needs a pen at least</div>
+<div>Scorekeeper needs a pen and clipboard</div>
+<div>Different colored markers can help</div>
+
+
+<div class="well">
+<div>Staples Staple Gun</div>
+<div>Staple gun: 5120-00-889-1796</div>
+<div>Staples, .5": 5315-00-889-2605</div>
+<div>Staples, 3/8": 5315-00-889-2604</div>
+</div>
+
+
+
+<div>Trash bags Trash bags</div>
+<div>clear, (33 gal): 8105-01-183-9768</div>
+<div>clear, (10 gal): 8105-01-195-8730</div>
+<div>Generally, black bags from the mess work great.</div>
+<div>Paper cups</div>
+<div>Water cooler Regular cooler (5 gal)</div>
+<div>Cooler: 7330-01-449-2319</div>
+<div>Rags 50 lb. Bundle</div>
+<div>Rags: 7920-00-205-1711</div>
+<div>Gerber tool</div>
+<div>Pliers</div>
+<div>M-16 Site Tool</div>
+<div>Wire cutter</div>
+<div>Screw driver</div>
+<div>Box cutter</div>
+<div>Staple guns</div>
+<div>Clean up Ensure range is clean</div>
+<div>Brass is picked up</div>
+<div>Some ranges want your shot brass</div>
+<div>Turn in borrowed range gear.</div>
+<div>Return all command equipment back to vehicles/storage</div>
+<div>Trash to dumpsters</div>
+<div>Smoking area</div>
+<div>chow garbage</div>
+<div>Flags are lowered</div>
+<div>Any watches secured</div>
+<div>Lock/secure any gates</div>
+<div>Return equipment</div>
+<div>Return any vehicles</div>
+<div>Log weapons back in</div>
+<div>Stow ammo/sign in</div>
+<div>Turn in score sheets</div>
+<div>Turn in muster sheets</div>
+<div>Post-Fires</div>
+<div>Ammo Expenditure Report</div>
+<div>Verify Records</div>
+<div>Print Cards</div> 
+
+
+
+
+
+
+</div> <!-- 4 columns -->
+</div> <!-- Row --> 
+</div> <!-- container-fluid --> 
+
+
+
+
+
+</body>
+
+
+
+
+
+</html>
